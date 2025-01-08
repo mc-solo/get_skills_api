1008343de39393098f0325d50d489af8d0ddec72
mc-solo <revatoel@gmail.com>
2025-01-08 09:46:13 +0300

    add: additional columns, edit: protected fillables at app/Models/User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,10 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'date of birth',
+        'department',
+        'hire date',
+        'address',
     ];
 
     /**
