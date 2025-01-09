b6d2f3647eb2e6188b1758cb58d8a51af33a4046
mc-solo <revatoel@gmail.com>
2025-01-09 09:27:27 +0300

    update: new columns to user table

---
+++ database/migrations/2025_01_08_134722_add_columns_to_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreignId('role_id')->constrained('roles')->cascadeOnDelete();
+            $table->date('date_of_birth')->nullable();  
+            $table->string('department')->nullable();
+            $table->string('address')->nullable();
+            $table->date('hire_date')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('role_id');
+            $table->dropColumn('role_permissions_id');
+            $table->dropColumn('Date of birth');
+            $table->dropColumn('Department');
+            $table->dropColumn('Address');
+            $table->dropColumn('Hire date');
+        });
+    }
+};
