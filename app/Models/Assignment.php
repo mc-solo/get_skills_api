1dd2b994245b6e911df79503601449d16d15faa8
mc-solo <revatoel@gmail.com>
2025-01-09 09:29:01 +0300

    add: more fillables

--- app/Models/Assignment.php
+++ app/Models/Assignment.php
@@ -8,5 +8,13 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Assignment extends Model
 {
     use HasFactory;
-    protected $fillable = ['assignment_name','description', 'due_date'];
+    protected $fillable = [
+    'assignment_name',
+    'description', 
+    'due_date',
+    'due_date',
+    'course_id',
+    'instructor_id'];
 }
+
+// todo:Relationship definitions
