7924246e2b598480b9222461674ae9a2c4a542b0
mc-solo <revatoel@gmail.com>
2025-01-21 14:43:00 +0300

    fix: unique phone number issue

--- database/migrations/2025_01_21_105945_create_phone_number_table.php
+++ database/migrations/2025_01_21_105945_create_phone_number_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('phone_number')->after('name');
+            $table->string('phone_number')->unique()->after('name');
         });
     }
 
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -15,8 +15,15 @@
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
+        
+        <!-- Phone Number -->
+        <div class="mt-4">
+            <x-input-label for="phone_number" :value="__('Phone Number')" />
+            <x-text-input id="phone_number" class="block mt-1 w-full" type="tel" name="phone_number" :value="old('phone_number')" required />
+            <x-input-error :messages="$errors->get('phone_number')" class="mt-2" />
+        </div>
 
-         <!-- Gender -->
+        <!-- Gender -->
         <div class="mt-4">
             <x-input-label for="gender" :value="__('Gender')"/>
             <select id="gender" name="gender" class="block mt-1 w-full">
@@ -47,7 +54,7 @@
             </select>
             <x-input-error :messages="$errors->get('educational_level')" class="mt-2" />
         </div>
-        
+
         <!-- Password -->
         <div class="mt-4">
             <x-input-label for="password" :value="__('Password')" />
