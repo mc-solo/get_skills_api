fb381e50da2bc8ebb79b8d5233f70e4ea9e51617
mc-solo <revatoel@gmail.com>
2025-01-21 13:16:50 +0300

    update: user model and add migration for gender

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,7 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'gender',
         'password',
         'date of birth',
         'department',
