08fbfad099f7b15fba614504895fb2157f52db64
mc-solo <revatoel@gmail.com>
2025-01-21 13:11:09 +0300

    update: validation logic for new users

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -32,12 +32,14 @@ class RegisteredUserController extends Controller
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
+            'gender'=> ['required', 'string', 'in:male, female, other'],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
+            'gender'=> $request->gender,
             'password' => Hash::make($request->password),
             'role'=>1,
         ]);
