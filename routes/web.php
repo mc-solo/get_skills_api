b3acef0b161d60dd1fa03de583bbdd7163b0b3f0
mc-solo <revatoel@gmail.com>
2025-02-07 13:59:51 +0300

    restore: web.php

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SocialAuthController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', function (){
+    return 'Get Slills API [ V1 ]';
+});
