aed9237157acf69e4c32b7f1f206ddf7ad081e9b
mc-solo <revatoel@gmail.com>
2025-02-10 10:07:26 +0300

    update: implement update and destroy

--- app/Http/Controllers/Api/V1/ReviewController.php
+++ app/Http/Controllers/Api/V1/ReviewController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers\Api\V1;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use App\Models\Review;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\V1\ReviewRequest;
+use Symfony\Component\HttpFoundation\Response as Res;
 
 class ReviewController extends Controller
 {
@@ -19,43 +22,49 @@ class ReviewController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(ReviewRequest $request)
     {
-        $validated =$request->validate([
-            'course_id' => 'required|exists:courses,id',
-            'rating' => 'required|integer|min:1|max:5',
-            'comment'=>'nullable|string|max:1000'
+        
+        $review = Review::create([
+            'user_id' => auth()->id(),
+            'course_id' => $request->course_id,
+            'rating' => $request->rating,
+            'comment' => $request->comment ?? null,
         ]);
 
-        $review = Review::create([
-            'user_id'=>auth()->id(),
-            'course_id'=>$validated['course_id'],
-            'rating'=>$validated['rating'],
-            'comment'=>$validated['comment'],
-        ]);
+        return response()->json($review, Res::HTTP_CREATED);
     }
+
+
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Review $review )
     {
-        //
+        return $review->load('user:id,name');   
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
-    {
-        //
+    public function update(ReviewRequest $request, Review $review) {
+        if($review->user_id !== auth()->id()){
+            return response()->json(['message' => 'Unauthorized'], Res::HTTP_UNAUTHORIZED);
+        }
+
+        $review->update($request->validated());
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
-    {
-        //
+    public function destroy(Review $review) {
+        if($review->user_id !== auth()->id()){
+            return response()->json(['message' => 'Unauthorized'], Res::HTTP_UNAUTHORIZED);
+        }
+
+        $review->delete();
+        return response()->json(null, Res::HTTP_NO_CONTENT);
     }
 }
