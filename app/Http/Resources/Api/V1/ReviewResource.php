4ce6f3994d518ad4edeb3c0fe4711d49614fdadb
mc-solo <revatoel@gmail.com>
2025-02-10 10:36:47 +0300

    create: a review resource

---
+++ app/Http/Resources/Api/V1/ReviewResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources\Api\V1;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ReviewResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'rating' => $this->rating,
+            'comment' => $this->comment,
+            'user' => [
+                'id' => $this->user->id,
+                'name' => $this->user->name,
+            ],
+            'course' => $this->course->id,
+            'created_at' => $this->created_at->toDateTimeString(),
+        ];
+    }
+}
