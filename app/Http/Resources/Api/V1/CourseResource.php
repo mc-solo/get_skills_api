ffef95b46602ef80e0ab9f776c507bd7ae121689
mc-solo <revatoel@gmail.com>
2025-02-12 10:04:13 +0300

    create: course resource

---
+++ app/Http/Resources/Api/V1/CourseResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources\Api\V1;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CourseResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'description' => $this->description,
+            'thumbnail' => $this->thumbnail,
+            'price' => $this->price,
+            'level' => $this->level,
+            'requirements' => $this->requirements,
+            'video_url' => $this->video_url,
+            'tags' => $this->tags,
+        ];
+    }
+}
