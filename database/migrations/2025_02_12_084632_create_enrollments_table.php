4de8a624212aaafcb708f29942bf9cd17abe4dba
mc-solo <revatoel@gmail.com>
2025-02-12 11:55:47 +0300

    create: enrollments migration

---
+++ database/migrations/2025_02_12_084632_create_enrollments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('enrollments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->foreignId('course_id')->constrained('courses')->cascadeOnDelete();
+            $table->decimal('progress', 5, 2)->default(0.00);
+            $table->boolean('is_completed')->default(false); //just to know if the course is finished
+            $table->enum('payment_status', ['pending', 'approved', 'declined'])->default('pending');
+            $table->string('transaction_id')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('enrollments');
+    }
+};
