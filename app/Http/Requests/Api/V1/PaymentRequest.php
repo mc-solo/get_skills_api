94c67f3e6f3169364f1d9742fa9be559c912804d
mc-solo <revatoel@gmail.com>
2025-02-13 15:12:48 +0300

    update: rules for payment request validation

---
+++ app/Http/Requests/Api/V1/PaymentRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests\Api\V1;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PaymentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'phone_number' => 'required|string|max:20',
+            'email' => 'required|email|255',
+            'tx_ref' => 'required|string|unique:transactions,column,tx_ref',
+            'currency' => 'required|string|max:3',
+            'status' => 'pending',
+        ];
+    }
+}
