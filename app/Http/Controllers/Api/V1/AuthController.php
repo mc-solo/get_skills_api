df1ab321f484ad25433d257236f1689df39b31ed
mc-solo <revatoel@gmail.com>
2025-02-16 22:31:00 +0300

    update: save data to db [doesn't work]

--- app/Http/Controllers/Api/V1/AuthController.php
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Controllers\Api\V1;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
 use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -14,15 +15,25 @@ class AuthController extends Controller
         $validated = $request->validate([
             'first_name' => 'required|string|max:255',
             'last_name' => 'required|string|max:255',
-            'phone_number' => 'required|string|unique:users',
-            'email' => 'required|email|unique:users',
+            'phone_number' => 'required|string|unique:users,phone_number',
+            'email' => 'required|email|unique:users,email',
             'password' => 'required|string|min:6',
         ]);
 
 
-        $user = User::create($validated);
-        return $user;
+        $validated['password'] = Hash::make($validated['password']);
 
+        // todo: debug why this is not working
+
+        // DB::table('users')->insert($validated);
+        $user = new User($validated);
+        $user->save();
+
+        if(!$user) {
+            return response()->json(['error'=>'User not created'], 500);
+        }
+
+        return response()->json(['message' => 'User created successfully', 'user' => $validated], 201);
     }
 
     public function login() {
