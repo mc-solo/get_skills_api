db7a5fda4772fa2c182f87df3c6351aa84396b9f
mc-solo <revatoel@gmail.com>
2025-02-18 09:45:27 +0300

    update: move index to api.php

--- routes/web.php
+++ routes/web.php
@@ -2,23 +2,23 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return response()->json([
-        'purpose' => 'backend api',
-        'version' => 'V1',
-        'authors' => [
-            [
-                "name" => "Wondwosen Asegid",
-                "email" => "wondwosen.asegid@gmail.com",
-                "phone" => "+251911563689",
-                "github" => "mc-solo"
-            ],
-            [
-                "name" => "Emmanuel Alem",
-                "email" => "emmaalem012@gmail.com",
-                "phone" => "+251904601186",
-                "github" => "Emmanuel-Alem"
-            ]
-        ]
-    ]);
-});
+// Route::get('/', function () {
+//     return response()->json([
+//         'purpose' => 'backend api',
+//         'version' => 'V1',
+//         'authors' => [
+//             [
+//                 "name" => "Wondwosen Asegid",
+//                 "email" => "wondwosen.asegid@gmail.com",
+//                 "phone" => "+251911563689",
+//                 "github" => "mc-solo"
+//             ],
+//             [
+//                 "name" => "Emmanuel Alem",
+//                 "email" => "emmaalem012@gmail.com",
+//                 "phone" => "+251904601186",
+//                 "github" => "Emmanuel-Alem"
+//             ]
+//         ]
+//     ]);
+// });
