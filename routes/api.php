d9e4473c8bf83942d2069efc14f6d7e2758e7360
mc-solo <revatoel@gmail.com>
2025-02-18 09:52:29 +0300

    restore: web.php

--- routes/api.php
+++ routes/api.php
@@ -23,26 +23,3 @@ Route::prefix('V1')->group(function () {
     Route::apiResource('reviews', ReviewController::class);
     Route::apiResource('courses', CourseController::class);
 });
-
-
-
-Route::get('/', function () {
-    return response()->json([
-        'purpose' => 'backend api',
-        'version' => 'V1',
-        'authors' => [
-            [
-                "name" => "Wondwosen Asegid",
-                "email" => "wondwosen.asegid@gmail.com",
-                "phone" => "+251911563689",
-                "github" => "mc-solo"
-            ],
-            [
-                "name" => "Emmanuel Alem",
-                "email" => "emmaalem012@gmail.com",
-                "phone" => "+251904601186",
-                "github" => "Emmanuel-Alem"
-            ]
-        ]
-    ]);
-});
