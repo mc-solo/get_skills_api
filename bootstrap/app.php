891b36aee65f0856ca1c510e3a1d7350a5c6fba4
mc-solo <revatoel@gmail.com>
2025-03-09 20:33:51 +0300

    update: registers hadle cors middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
             'auth:sanctum' =>\Illuminate\Auth\Middleware\Authenticate::class,
+            \Illuminate\Http\Middleware\HandleCors::class,
         ]);
 
         $middleware->append(EnsureFrontendRequestsAreStateful::class);
