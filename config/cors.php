589447e5913f0f4a5916579a3ab46274e9676a81
mc-solo <revatoel@gmail.com>
2025-03-09 21:07:59 +0300

    fix: sets supports_credentials to true in cors.php

--- config/cors.php
+++ config/cors.php
@@ -7,8 +7,8 @@ return [
         env('FRONTEND_URL', 'http://localhost:3000'),
     ],
     'allowed_origins_patterns' => [],
-    'allowed_headers' => [],
+    'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 ];
